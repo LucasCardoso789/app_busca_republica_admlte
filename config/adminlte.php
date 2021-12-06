2bb56da8024a5b17a360a13dc55e2fe529505e1d
Lucas Mateus Cruz Cardoso <lucas.cardoso@campus.unimontes.int>
2021-12-06 18:53:55 -0300

    Configuração no Layout

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 3',
+    'title' => 'Busca Republica',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -45,8 +45,8 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo' => '<b>Busca</b>República',
+    'logo_img' => 'vendor/adminlte/dist/img/busca_republica.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
---
+++ resources/views/painel_principal.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'Busca República')
+
+
+@section('content_header')
+    <h1>Painel</h1>
+@stop
+
+@section('content')
+    <p>Bem vindo ao Painel Principal do Sistema Busca República</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
